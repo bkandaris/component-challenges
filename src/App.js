9d51bb30e700b79c07204476f7e1116c0d192a5b
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-12-16 23:33:44 -0500

    mostly done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Profile from './challengeOne_Profile/profile';
 
+
+
+
 function App() {
+
+
 	return (
 		<div>
 			<Route exact path="/profile" component={Profile} />
--- src/challengeOne_Profile/profile.js
+++ src/challengeOne_Profile/profile.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import pinsvg from './assets/pinIcon.svg'
+import msgsvg from './assets/messageIcon.svg';
+import terminalIcon from './assets/terminalIcon.svg'
 
 const Profile = () => {
-	return <div> Do the Thing </div>;
+
+
+	const [person, setPerson ] = useState(null)
+	const [friends, setFriends ] = useState(null)
+	const [buttonOn, setButtonOn] = useState(true)
+
+	const addFriend = () => {
+		setFriends(friends+1)
+		setButtonOn(!buttonOn)
+	}
+
+	const deleteFriend = () => {
+		setFriends(friends-1)
+		setButtonOn(!buttonOn)
+	}
+
+	useEffect(()=> {
+		axios.get("https://api.github.com/users/bkandaris")
+		.then((res) => {
+			setPerson(res.data)
+			setFriends(res.data.followers)
+			console.log('data', res)
+		})
+		.catch(err => {console.log(err)})
+	}, [])
+
+	return(
+	<div className="mainDiv"> 
+		<div className="headerDiv" >
+			<img className="profileImage" src={person.avatar_url}/>
+			<p className="name">{person.name}</p>
+			<p className="location"><img src={pinsvg} alt={"pin location"}  />{person.location}</p>
+		</div>
+		<div className="friendsDiv" >
+			<div className="friendsMiniDiv" >
+				<h3>Friends</h3>
+				<p className="friendsNumbers">{friends}</p>
+			</div>
+			<div className="friendsMiniDiv">
+				<h3>Repos</h3>
+				<p className="friendsNumbers">{person.public_repos}</p>
+			</div>
+			<div className="friendsMiniDiv">
+				<h3>Bugs</h3>
+				<p className="friendsNumbers">{person.following}</p>
+			</div>
+		</div>
+		<div className="buttonWrapper" >
+		{ buttonOn ?
+			<button className="upButton codeTogether" onClick={()=> {addFriend()}}>
+				<img className="buttonImg" src={terminalIcon} alt="terminal icon" />
+				Code Together
+			</button>
+				:
+			<button className="downButton codeTogether"onClick={()=> {deleteFriend()}}>
+				<img className="buttonImg" src={terminalIcon} alt="terminal icon" />
+				Code Together
+			</button>
+			}
+			<div className="msgWrapper">
+				<img  src={msgsvg} alt="message icon"/>
+			</div>
+		</div>
+	 </div>);
 };
 
 export default Profile;
