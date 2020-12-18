ba4a0bbf3716ef1ed5831f1143c73502646831c0
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-12-17 20:53:29 -0500

    fixed border

--- src/challengeOne_Profile/profile.js
+++ src/challengeOne_Profile/profile.js
@@ -40,7 +40,6 @@ const Profile = () => {
 	<motion.div     animate={{
 		scale: [1, 2, 2, 1, 1],
 		rotate: [0, 0, 270, 270, 0],
-		borderRadius: ["20%", "20%", "50%", "50%", "20%"],
 	  }}  className="mainDiv"> 
 		<div className="headerDiv" >
 			<img className="profileImage" src={person.avatar_url}/>
